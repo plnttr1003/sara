6cc890d54a282e4ddea75458134159900e2f381b
s.topunov <s.topunov@rambler-co.ru>
2017-07-27 18:49:15 +0300

    changes

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,7 @@ var Promo = require('../models/main.js').Promo;
 var __appdir = path.dirname(require.main.filename);
 
 exports.index = function(req, res) {
-	Promo.find().where('title').nor([{'status': 'hidden'}, {'status': 'out'}]).sort('position').limit(15).exec(function(err, promo) {
+	Promo.find().where('title').nor([{'status': 'hidden'}, {'status': 'out'}]).sort('-date').limit(15).exec(function(err, promo) {
 		res.render('main', {promo: promo});
 	});
 }
